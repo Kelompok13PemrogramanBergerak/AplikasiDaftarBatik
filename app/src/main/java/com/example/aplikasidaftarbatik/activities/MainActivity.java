e010a89215f1803fc14d773f6118ae677cf8b090
FiqihAlfito <fiqihalfito1@gmail.com>
2020-11-25 09:11:56 +0700

    initial commit

--- app/src/main/java/com/example/aplikasidaftarbatik/activities/MainActivity.java
+++ app/src/main/java/com/example/aplikasidaftarbatik/activities/MainActivity.java
@@ -28,9 +28,7 @@ public class MainActivity extends AppCompatActivity {
     RecyclerView mRecyclerView;
     List<Batik> dataBatik;
     BatikAdapter batikAdapter;
-
-    //wendi
-
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
